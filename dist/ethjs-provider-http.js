9acaa9d7446f984cd7ab77d1f1dd4daaa51148db
legobt <6wbvkn0j@anonaddy.me>
2023-11-08 03:20:18 +0000

    babel-*@6 -> @babel/*@^7
    - BREAKING: add peerDependency @babel/runtime@^7.0.0

--- dist/ethjs-provider-http.js
+++ dist/ethjs-provider-http.js
@@ -97,7 +97,6 @@ module.exports = __webpack_require__(1);
  */
 
 // workaround to use httpprovider in different envs
-
 var XHR2 = __webpack_require__(2);
 
 /*
@@ -129,7 +128,7 @@ timeout
  * InvalidResponseError helper for invalid errors.
  */
 function invalidResponseError(request, host) {
-  var responseError = new Error('[ethjs-provider-http] Invalid JSON RPC response from provider\n    host: ' + host + '\n    response: ' + String(request.responseText) + ' ' + JSON.stringify(request.responseText, null, 2) + '\n    responseURL: ' + request.responseURL + '\n    status: ' + request.status + '\n    statusText: ' + request.statusText + '\n  ');
+  var responseError = new Error("[ethjs-provider-http] Invalid JSON RPC response from provider\n    host: " + host + "\n    response: " + String(request.responseText) + " " + JSON.stringify(request.responseText, null, 2) + "\n    responseURL: " + request.responseURL + "\n    status: " + request.status + "\n    statusText: " + request.statusText + "\n  ");
   responseError.value = request;
   return responseError;
 }
@@ -144,7 +143,6 @@ function HttpProvider(host, timeout) {
   if (typeof host !== 'string') {
     throw new Error('[ethjs-provider-http] the HttpProvider instance requires that the host be specified (e.g. `new HttpProvider("http://localhost:8545")` or via service like infura `new HttpProvider("http://ropsten.infura.io")`)');
   }
-
   var self = this;
   self.host = host;
   self.timeout = timeout || 0;
@@ -165,7 +163,6 @@ HttpProvider.prototype.sendAsync = function (payload, callback) {
   request.timeout = self.timeout;
   request.open('POST', self.host, true);
   request.setRequestHeader('Content-Type', 'application/json');
-
   request.onreadystatechange = function () {
     if (request.readyState === 4 && request.timeout !== 1) {
       var result = request.responseText; // eslint-disable-line
@@ -176,22 +173,18 @@ HttpProvider.prototype.sendAsync = function (payload, callback) {
       } catch (jsonError) {
         error = invalidResponseError(request, self.host);
       }
-
       callback(error, result);
     }
   };
-
   request.ontimeout = function () {
-    callback('[ethjs-provider-http] CONNECTION TIMEOUT: http request timeout after ' + self.timeout + ' ms. (i.e. your connect has timed out for whatever reason, check your provider).', null);
+    callback("[ethjs-provider-http] CONNECTION TIMEOUT: http request timeout after " + self.timeout + " ms. (i.e. your connect has timed out for whatever reason, check your provider).", null);
   };
-
   try {
     request.send(JSON.stringify(payload));
   } catch (error) {
-    callback('[ethjs-provider-http] CONNECTION ERROR: Couldn\'t connect to node \'' + self.host + '\': ' + JSON.stringify(error, null, 2), null);
+    callback("[ethjs-provider-http] CONNECTION ERROR: Couldn't connect to node '" + self.host + "': " + JSON.stringify(error, null, 2), null);
   }
 };
-
 module.exports = HttpProvider;
 
 /***/ }),
